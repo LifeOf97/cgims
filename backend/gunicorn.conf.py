887e26fc1a46812bde0315eccfecb0d386aa6383
realestkma <kelvinmayoayeni@gmail.com>
2022-08-01 17:09:39 +0100

    Added nginx to serve as a reverse proxy

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -4,11 +4,11 @@ bind = "0.0.0.0:8000"
 # bind = "unix:/run/cgims.sock"
 workers = multiprocessing.cpu_count() * 2 + 1
 reload_engine = "inotify"
-# group = "realestKMA"
-# user = "realestKMA"
+group = "cgims"
+user = "cgims"
 reload = True
 
 # logs
 loglevel = "debug"
-# accesslog = "logs/gunicorn/cgmis.access"
-# errorlog = "logs/gunicorn/cgims.error"
+accesslog = "logs/gunicorn/cgmis.access"
+errorlog = "logs/gunicorn/cgims.error"
--- backend/src/settings.py
+++ backend/src/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
