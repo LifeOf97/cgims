608079b95207f3f3ff0480edc8b61fbc2a8a00c0
realestkma <kelvinmayoayeni@gmail.com>
2022-08-03 16:04:24 +0100

    cu

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,14 +1,14 @@
-import multiprocessing
+# import multiprocessing
 
-bind = "0.0.0.0:8000"
-# bind = "unix:/run/cgims.sock"
-workers = multiprocessing.cpu_count() * 2 + 1
-reload_engine = "inotify"
-group = "cgims"
-user = "cgims"
-reload = True
+# bind = "0.0.0.0:8000"
+# # bind = "unix:/run/cgims.sock"
+# workers = multiprocessing.cpu_count() * 2 + 1
+# reload_engine = "inotify"
+# group = "cgims"
+# user = "cgims"
+# reload = True
 
-# logs
-loglevel = "debug"
-accesslog = "logs/gunicorn/cgmis.access"
-errorlog = "logs/gunicorn/cgims.error"
+# # logs
+# loglevel = "debug"
+# accesslog = "logs/gunicorn/cgmis.access"
+# errorlog = "logs/gunicorn/cgims.error"
